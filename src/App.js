89833bb3df12acaeaf67a429ba9be190e03982a7
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2020-01-07 18:33:29 -0800

    streamlined react imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import NavHeader from './components/navHeader/navHeader'
 import Square from './components/square/square'
 import './App.css'
@@ -10,10 +10,10 @@ function App () {
 
   // In class components we use a state object. In function components we use individual state variables
   // React.useState takes an initial value and returns an array with the state variable and it's setter
-  const [palette, setPalette] = React.useState(palettes[0])
+  const [palette, setPalette] = useState(palettes[0])
 
   return (
-    <React.Fragment>
+    <Fragment>
       <NavHeader message='This is the NavHeader component'>
         {/* Anything between a component's open and close tags will be automatically passed to it
          as the 'children' array in the component's 'props' object */}
@@ -28,7 +28,7 @@ function App () {
       <button onClick={() => palette[0] === 'blue' ? setPalette(palettes[1]) : setPalette(palettes[0])}>
           Switch Palette
       </button>
-    </React.Fragment>
+    </Fragment>
   )
 }
 
